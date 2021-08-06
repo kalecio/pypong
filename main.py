938b706944649e879344ed8ce592b99527773c1c
Kalecio Pereira <kalecio.santana@gmail.com>
2021-08-06 18:30:09 -0300

    feat: create players and ball

--- main.py
+++ main.py
@@ -6,6 +6,33 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Player A
+player_a = turtle.Turtle()
+player_a.speed(0)
+player_a.shape("square")
+player_a.color("white")
+player_a.shapesize(stretch_wid=5, stretch_len=1)
+player_a.penup()
+player_a.goto(-350, 0)
+
+
+# Player B
+player_b = turtle.Turtle()
+player_b.speed(0)
+player_b.shape("square")
+player_b.color("white")
+player_b.shapesize(stretch_wid=5, stretch_len=1)
+player_b.penup()
+player_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+player_b.goto(0, 0)
+ball.penup()
+
 # Main game loop
 
 while True:
